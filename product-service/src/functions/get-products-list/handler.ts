feed9edf5322508ce3d550d1fdae8353d3598fa5
Pavel_Yatskevich <Pavel_yatskevich@epam.com>
2021-06-17 16:54:29 +0200

    add get products list service

---
+++ product-service/src/functions/get-products-list/handler.ts
@@ -0,0 +1,12 @@
+import 'source-map-support/register';
+
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import { formatJSONResponse } from '@libs/apiGateway';
+import { middyfy } from '@libs/lambda';
+
+import products from './mock'
+import schema from './schema';
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => formatJSONResponse(products)
+
+export const main = middyfy(getProductsList);
